53ea0f82c3ecbf2b81ff582e8fe81df5e7fbd72b
ArtemVikharev <webavikharev@gmail.com>
2025-11-06 16:24:41 +0300

    добавил первую макрозадачу

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-function createQueryTask() {}
+function createQueryTask() {
+  setTimeout(() => {
+    console.log("Создание первой макрозадачи: setTimeout 1");
+
+    Promise.resolve().then(() => {
+      console.log("Микрозадача 1: обработка промиса внутри setTimeout 1");
+    });
+
+    function changeColor() {
+      document.querySelector(".main-title").style = ` color: red;`;
+      console.log(
+        "Рендер задача внутри первой макрозадачи: Изменение цвета заголовка"
+      );
+    }
+    requestAnimationFrame(() => {
+      changeColor();
+    });
+  }, 1000);
+}
